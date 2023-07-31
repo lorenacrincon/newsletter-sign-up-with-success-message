371546500755a1a554d4a8b6825a39c5bad142c7
Lorena Rincon <lorec.rincon@gmail.com>
2023-07-31 11:10:53 -0300

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Attribution from "./assets/components/Attribution";
+import SignUp from "./assets/components/SignUp";
+
+function App() {
+  return (
+    <>
+      <SignUp />
+      <Attribution />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/Attribution.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Attribution() {
+  return (
+    <div class="attribution">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="#">Lorena Rincón</a>.
+    </div>
+  );
+}
+
+export default Attribution;
